1db0c9504ba31e5baca1a67184013e16322997e8
LucasRomanet <lucas@romanet.dev>
2023-06-22 22:10:28 +0200

    Save

--- app/src/context/player/PlayerConsumer.js
+++ app/src/context/player/PlayerConsumer.js
@@ -2,11 +2,10 @@ import { useContext } from 'react';
 import { PlayerContext } from './PlayerContext';
 
 export default function withPlayer(Component) {
-    const {player, setPlayer} = useContext(PlayerContext);
+    const { player, setPlayer } = useContext(PlayerContext);
 
     function ComponentWithPlayerProp(props) {
-        return <Component {...props} 
-        />
+        return <Component {...props} player setPlayer />
     }
 
     return ComponentWithPlayerProp;
